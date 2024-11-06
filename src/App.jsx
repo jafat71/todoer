3414e0867117366b4c76d37635ab11da232a209f
jzafvt771 <ijmm7171@gmail.com>
2024-11-05 23:47:40 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AppRouter from "./routes";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import CalendarForm from "@/components/custom/CalendarForm";
+
+const Home = () => {
+    return (
+        <>
+            <CalendarForm/>
+        </>
+    )
+}
+
+export default Home;
